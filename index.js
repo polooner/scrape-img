19aa910707383297d2bdff62f8311ce7988fa929
Filip Wojda <filip.wojda001@stu.bmcc.cuny.edu>
2023-03-13 13:07:51 -0400

    final working

--- index.js
+++ index.js
@@ -1,55 +1,72 @@
 import puppeteer from 'puppeteer';
+import fetch from 'node-fetch';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
 
-function delay(time) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, time);
-  });
-}
+dotenv.config();
 
 async function downloadImages() {
+  let data = {};
+
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 720 });
   console.log('page loading');
-  await page.goto('https://evergreeninvestments.co/daily-dose-of-deals/', {
+  await page.goto(process.env.WEBSITE, {
     waitUntil: 'networkidle0',
     timeout: 0,
   });
   console.log('page loaded');
   //Nitro links always work, src attribute does not because those are
-  //dynamically assigned when a page with that element is shown
-  let imgTags = await page.$$('. lazyloaded');
-  for (let link of imgTags) {
-    const attr = await page.evaluate(
-      (el) => el.getAttribute('nitro-lazy-src'),
-      link
+  //dynamically assigned when a page with that element is shown - unless
+  //streetview failed, which gives it a faulty src href
+  let numberOfImages = await page.$$eval(
+    '.property-image',
+    (divs) => divs.length
+  );
+  console.log(numberOfImages);
+
+  //iterate over number of property divs, save img tags nitro-lazy-src links
+  for (let i = 1; i < numberOfImages + 1; i++) {
+    let aTag = await page.$x(
+      `/html/body/div[1]/div/div/div[1]/div[5]/a[${i}]/div/div[1]/img`
     );
-    console.log(attr);
+    let src = await page.evaluate(
+      (el) => el.getAttribute('nitro-lazy-src'),
+      aTag[0]
+    );
+    console.log(src);
+    if (!src) {
+      //Most of the time, this means StreetView picture is empty
+      //and nitro-lazy-src does not exist. It still downloads it to keep hierarchy
+      let srcNotNitro = await page.evaluate(
+        (el) => el.getAttribute('src'),
+        aTag[0]
+      );
+      saveImg(i, srcNotNitro);
+      console.log(`Saved file number ${i}`);
+    } else {
+      data[i] = src;
+      saveImg(i, src);
+      console.log(`Saved file number ${i}`);
+    }
   }
+  //TODO: check if file exists by comparing links to .json file.
+  //will be helpful and time-saving if we want to keep this workflow
 
-  // await page.evaluate(() => {
-  //   const links = Array.from(document.querySelectorAll('img.lazyloaded')); // get all links
-  //   return links.map((link) => link.nitrolazysrc); // return the first 20 links as an array of hrefs
-  // });
-  // await delay(10000);
+  let jsonData = JSON.stringify(data);
+  fs.writeFileSync('all_links.json', jsonData);
 
-  await page.screenshot({ encoding: 'binary', path: './image.png' });
   await browser.close();
-
-  // const imgUrls = await page.$$eval('.', (links) => {
-  //   return links.map((link) => link.href);
-  // });
-
-  // for (const url of imgUrls) {
-  //   await page.goto(url, { waitUntil: 'networkidle2' });
-  //   const imgSrc = await page.$eval('img', (img) => img.src);
-  //   await page.goto(imgSrc, { waitUntil: 'networkidle2' });
-  //   const buffer = await page.screenshot({ encoding: 'binary' });
-  //   // You can save the image buffer to a file using a Node.js file system module
-  //   // Example: fs.writeFileSync('image.png', buffer);
-  // }
-
-  // await browser.close();
 }
 
 downloadImages();
+
+const saveImg = async (index, srcLink) => {
+  const fileName = index.toString() + '.jpg';
+  const filePath = './images/' + fileName;
+  const response = await fetch(srcLink);
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  fs.writeFileSync(filePath, buffer);
+};
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Download all images of a singular nested path. This example uses one XPath with incrementing &lt;a>&lt;/a> tags.
+
+## THIS REPO USES YARN
+
+## How to use:
+
+## 1. Copy repo locally
+
+## 2. In the terminal, run `yarn install`
+
+## 3. touch .env
+
+## 4. cp .env.example .env - to copy the example .env into your .env
+
+## 5. Specify website, change XPaths as desired
